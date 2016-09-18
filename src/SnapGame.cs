f3616e30a075c56c5f693344714eddada19a1a1d
josephsim <joseph_sim19@hotmail.com>
2016-09-19 01:35:29 +0800

    Make changes to the font for player score

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -14,6 +14,7 @@ namespace CardGames
 			SwinGame.LoadSoundEffectNamed("Slap", "slap.wav");
 			SwinGame.LoadSoundEffectNamed("bananaSlap", "bananaslap.wav");
 			SwinGame.LoadSoundEffectNamed("Punch", "punch.wav");
+			SwinGame.LoadFontNamed ("GameFont", "Chunkfive.otf", 24);
         }
 
 		/// <summary>
@@ -62,9 +63,10 @@ namespace CardGames
 			Card top = myGame.TopCard;
 			if (top != null)
 			{
+				//TODO: Change score font
 				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.RoyalBlue, 0, 20);
-				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.RoyalBlue, 0, 30);
-				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.RoyalBlue, 0, 40);
+				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.White, "GameFont", 0, 30);
+				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.White, "GameFont", 0, 60);
 				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 521, 153);
 			}
 			else
